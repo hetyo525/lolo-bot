20d2fa511f0410c46dda0f09b7f36de8c94ba625
hetyo525 <hetyoinfo@gmail.com>
2020-05-16 06:21:01 +0900

    fix(imap): セッションエラー時にコネクションを貼り直す(#5)

--- src/providers/imap.js
+++ src/providers/imap.js
@@ -9,11 +9,10 @@ const simpleParser = require('mailparser').simpleParser;
 module.exports = class extends Provider {
   constructor(...args) {
     super(...args, { description: 'IMAP Client' });
-    this.imap = null;
   }
 
   async init() {
-    this.imap = await Imaps.connect({
+    this.config = {
       imap: {
         user: process.env['IMAP_MAIL_ADDRESS'],
         password: process.env['IMAP_MAIL_PASSWORD'],
@@ -22,7 +21,42 @@ module.exports = class extends Provider {
         tls: true,
         authTimeout: 10000,
       },
-    });
+    };
+    await this.connect();
+  }
+
+  /**
+   * https://github.com/chadxz/imap-simple/issues/65#issuecomment-604142582
+   * @returns {Promise<void>}
+   */
+  async connect() {
+    const me = this;
+    try {
+      if (!this._c) {
+        this._c = await Imaps.connect(this.config);
+        console.info(`IMAP connected to ${this.config.imap.host}`);
+        this._c.on('error', (err) => {
+          // THIS IS REQUIRED not to crash the process on socket error
+          console.error(err);
+        });
+        this._c.imap.on('close', () => {
+          this._c = null;
+          setTimeout(() => {
+            console.debug('IMAP reconnecting after error');
+            me.connect();
+          }, this.config.imap.authTimeout);
+        });
+      }
+      const boxName = process.env['MAIL_BOX_NAME'];
+      await this._c.openBox(boxName);
+      console.info(`IMAP ${boxName} opened`);
+    } catch (ex) {
+      console.error(ex);
+      setTimeout(() => {
+        console.debug('IMAP initial reconnect');
+        me.connect();
+      }, this.config.imap.authTimeout);
+    }
   }
 
   /**
@@ -31,8 +65,8 @@ module.exports = class extends Provider {
    */
   async fetchNewestMails() {
     // 対象のメールボックスから「未読」のメールを取得する（取得時に「既読」にする）
-    await this.imap.openBox(process.env['MAIL_BOX_NAME']);
-    const mails = await this.imap.search(['UNSEEN'], { bodies: ['HEADER', 'TEXT', ''], markSeen: true });
+    await this._c.openBox(process.env['MAIL_BOX_NAME']);
+    const mails = await this._c.search(['UNSEEN'], { bodies: ['HEADER', 'TEXT', ''], markSeen: true });
 
     return await Promise.all(
       mails.map((mail) => {
